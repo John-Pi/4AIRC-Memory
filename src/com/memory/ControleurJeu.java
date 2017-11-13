60d59802a3ad0ded1697be485c404eeb14a04371
John-Pi <john.picolton@gmail.com>
2017-11-13 14:34:09 +0100

    controlleur

--- src/com/memory/ControleurJeu.java
+++ src/com/memory/ControleurJeu.java
@@ -3,13 +3,14 @@ package com.memory;
 public class ControleurJeu {
 
     private JeuMemory jeuMemory;
+    private Paquet paquet;
 
     public ControleurJeu(){
+        jeuMemory = new JeuMemory();
 
     }
 
     public Paquet RecupPaquet(){
-
         return null;
     }
 
