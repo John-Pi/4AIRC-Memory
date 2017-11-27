5987165ff6d2d13133851d6c54fe763bba867642
JeremySanz <32543354+JeremySanz@users.noreply.github.com>
2017-11-27 15:36:11 +0100

    Fin de partie cmd
    Gestion de la fin de la partie et de l'option rejouer ou non en cmd

--- src/com/memory/ControleurJeu.java
+++ src/com/memory/ControleurJeu.java
@@ -6,7 +6,8 @@ public class ControleurJeu {
     private Paquet paquet = null;
     private MemoryCMDLine memoryCMDLine;
     private int state;
-    private Carte carte1 = null,carte2 = null;
+    private Carte carte1 = null, carte2 = null;
+    private boolean Rejouer = false;
 
     public ControleurJeu() {
     }
@@ -19,54 +20,56 @@ public class ControleurJeu {
     public void RetournerCarte(int x, int y) {
     }
 
-    public void LauchGame() {
-        jeuMemory = new JeuMemory();
-        RecupPaquet();
-        memoryCMDLine = new MemoryCMDLine();
-        memoryCMDLine.setPaquet(paquet);
-        memoryCMDLine.afficheStart();
-        Carte tempcarte;
-        state = State.startTry;
-        int[] tab;
+    public void LauchGameCMD() {
 
-        while (jeuMemory.getPairesRestantes() > 0 && jeuMemory.getCoupRestants() > 0) {
+        do {
+            jeuMemory = new JeuMemory();
+            RecupPaquet();
+            memoryCMDLine = new MemoryCMDLine();
+            memoryCMDLine.setPaquet(paquet);
+            memoryCMDLine.afficheStart();
+            Carte tempcarte;
+            state = State.startTry;
+            int[] tab;
 
-            switch (state) {
-                case State.startTry:
-                    memoryCMDLine.affichageJeu();
-                    tab = memoryCMDLine.retournerCarte();
-                    tempcarte = jeuMemory.FindCarte(tab[0], tab[1]);
-                    if (jeuMemory.IsValide(tempcarte) == State.CarteValide) {
-                        jeuMemory.RetournerCarte(tempcarte);
-                        state = State.firstCardChoosed;
-                        carte1 = tempcarte;
-                    }else {
-                        memoryCMDLine.afficheErreur();
-                    }
-                    break;
-                case State.firstCardChoosed:
-                    memoryCMDLine.affichageJeu();
-                    tab = memoryCMDLine.retournerCarte();
-                    tempcarte = jeuMemory.FindCarte(tab[0], tab[1]);
-                    if (jeuMemory.IsValide(tempcarte) == State.CarteValide) {
-                        jeuMemory.RetournerCarte(tempcarte);
-                        state = State.pairControl;
-                        carte2 = tempcarte;
-                    }else {
-                        memoryCMDLine.afficheErreur();
-                    }
-                    break;
-                case State.pairControl:
-                    if (jeuMemory.SontPaires(carte1, carte2)){
-                        state = State.coupGagnant;
-                    }else {
-                        state = State.coupPerdant;
-                    }
-                    break;
+            while (jeuMemory.getPairesRestantes() > 0 && jeuMemory.getCoupRestants() > 0) {
+
+                switch (state) {
+                    case State.startTry:
+                        memoryCMDLine.affichageJeu();
+                        tab = memoryCMDLine.retournerCarte();
+                        tempcarte = jeuMemory.FindCarte(tab[0], tab[1]);
+                        if (jeuMemory.IsValide(tempcarte) == State.CarteValide) {
+                            jeuMemory.RetournerCarte(tempcarte);
+                            state = State.firstCardChoosed;
+                            carte1 = tempcarte;
+                        } else {
+                            memoryCMDLine.afficheErreur();
+                        }
+                        break;
+                    case State.firstCardChoosed:
+                        memoryCMDLine.affichageJeu();
+                        tab = memoryCMDLine.retournerCarte();
+                        tempcarte = jeuMemory.FindCarte(tab[0], tab[1]);
+                        if (jeuMemory.IsValide(tempcarte) == State.CarteValide) {
+                            jeuMemory.RetournerCarte(tempcarte);
+                            state = State.pairControl;
+                            carte2 = tempcarte;
+                        } else {
+                            memoryCMDLine.afficheErreur();
+                        }
+                        break;
+                    case State.pairControl:
+                        if (jeuMemory.SontPaires(carte1, carte2)) {
+                            state = State.coupGagnant;
+                        } else {
+                            state = State.coupPerdant;
+                        }
+                        break;
                     case State.coupGagnant:
                         memoryCMDLine.affichageJeu();
                         jeuMemory.paireTrouvee(carte1, carte2);
-                        memoryCMDLine.coupJoue( jeuMemory.getCoupRestants(), jeuMemory.getPairesRestantes() , true);
+                        memoryCMDLine.coupJoue(jeuMemory.getCoupRestants(), jeuMemory.getPairesRestantes(), true);
                         state = State.startTry;
                         break;
                     case State.coupPerdant:
@@ -76,13 +79,19 @@ public class ControleurJeu {
                         carte1 = null;
                         carte2 = null;
                         state = State.startTry;
-                        memoryCMDLine.coupJoue( jeuMemory.getCoupRestants(), jeuMemory.getPairesRestantes() , false);
+                        memoryCMDLine.coupJoue(jeuMemory.getCoupRestants(), jeuMemory.getPairesRestantes(), false);
                         break;
-
+                }
             }
-
-
-        }
+            if (jeuMemory.getPairesRestantes() == 0 && jeuMemory.getCoupRestants() > 0) {
+                memoryCMDLine.partieGagnee();
+                Rejouer = memoryCMDLine.rejouer();
+            } else {
+                memoryCMDLine.partiePerdue();
+                Rejouer = memoryCMDLine.rejouer();
+            }
+        } while (Rejouer);
+        memoryCMDLine.messageFin();
     }
 
 }
